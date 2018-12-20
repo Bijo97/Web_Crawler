d531eff7eb30079bc949158a7415c56e467025be
Albert Pratomo <i406448@fhict.nl>
2018-12-20 16:22:13 +0100

    WCARest:hasUrlStringAsRequest_1

---
+++ src/test/java/WCARestTest.java
@@ -0,0 +1,24 @@
+import org.jsoup.Jsoup;
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+
+public class WCARestTest {
+    @Test
+    public void hasUrlStringAsRequest(){
+        // ARRANGE
+        String url = "www.google.com";
+        // Create SUT
+        WCARest SUT = new WCARest(url);
+
+        // ACT
+
+        // ASSERT
+        assertNotEquals("WCARest should have a String request",url,SUT.getRequest());
+    }
+}
+
+
+
