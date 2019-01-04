92f3ee484c4fae35226327c32d8a4a4f7552762b
Amelia Kezia Gunawan,Amel A <a.kezia@student.fontys.nl>
2019-01-04 18:45:47 +0100

    Book : ISBNFormatIsCorrect

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Book {
     private Media media;
@@ -6,11 +7,37 @@ public class Book {
     private String publisher;
     private String isbn;
 
-    public Book(Media media, List<String> authors, String publisher, String isbn) {
-        this.media = media;
-        this.authors = authors;
-        this.publisher = publisher;
-        this.isbn = isbn;
+    public Book(Media media, List<String> authors, String publisher, String isbn) throws NoDataItemsException, WrongFormatException {
+        Pattern p = Pattern.compile("\\d{3}-\\d{10}");
+
+        Boolean checkAuthors, checkPublisher, checkISBN, checkISBNFormat;
+        checkAuthors = authors.isEmpty();
+        checkPublisher = publisher.trim().isEmpty();
+        checkISBN = isbn.trim().isEmpty();
+        checkISBNFormat = p.matcher(isbn).matches();
+
+        if(!checkAuthors){
+            if(!checkPublisher){
+                if(!checkISBN){
+                    if(checkISBNFormat){
+                        this.media = media;
+                        this.authors = authors;
+                        this.publisher = publisher;
+                        this.isbn = isbn;
+                    } else {
+                        throw new WrongFormatException("ISBN Format should be ddd-dddddddddd  (3)-(10)");
+                    }
+
+                } else {
+                    throw new NoDataItemsException("ISBN should not be empty");
+                }
+            } else {
+                throw new NoDataItemsException("Publisher should not be empty.");
+            }
+        } else {
+            throw new NoDataItemsException("Author should not be empty.");
+        }
+
     }
 
     public List getAuthors() {
--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -31,7 +31,30 @@ public class BookTest {
 
     }
 
+    @Test (expected = NoDataItemsException.class)
+    public void artistsPublisherAndISBNAreNotEmpty() throws WrongFormatException, NoDataItemsException {
+        media = new Media(genre,format,year,name);
+        List<String> authors = new ArrayList<>();
 
+        authors.add("Robert C. Martin");
+        String publisher = "Prentice Hall";
+        String isbn = "";
+
+        Book book = new Book(media,authors,publisher,isbn);
+
+    }
+
+    @Test (expected = WrongFormatException.class)
+    public void ISBNFormatIsCorrect() throws WrongFormatException, NoDataItemsException {
+        media = new Media(genre,format,year,name);
+        List<String> authors = new ArrayList<>();
+
+        authors.add("Robert C. Martin");
+        String publisher = "Prentice Hall";
+        String isbn = "007-60920469811";
+        Book book = new Book(media,authors,publisher,isbn);
+
+    }
 
     public static boolean compareList(List ls1,List ls2){
         System.out.println(ls1.toString());
