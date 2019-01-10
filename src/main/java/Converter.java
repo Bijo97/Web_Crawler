3cd0826ba69eab8f68eb3a9c4e21bb02adabecb4
Albert Pratomo <i406448@fhict.nl>
2019-01-10 12:09:02 +0100

    Converter: shouldReturnJSONStringOfMetadataParameters

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -2,11 +2,20 @@ import com.owlike.genson.Genson;
 
 public class Converter{
 
-        public String convert(Media m) {
+        public String convertMedia(Media m) {
             Genson genson = new Genson();
 
             String json = genson.serialize(m);
-//            genson.deserialize(json, String[].class);
+
+            return json;
+        }
+
+        public String convertMetadata(String _strategy,int _numberOfPage,int _timeElapsed,int _searchDepth) {
+
+            String json = "{\"numberOfPage\":\""+_numberOfPage+
+                    "\",\"searchDepth\":\""+_searchDepth+
+                    "\",\"strategy\":\""+_strategy+
+                    "\",\"timeElapsed\":\""+_timeElapsed+"\"}";;
 
             return json;
         }
--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -17,7 +17,7 @@ public class ConverterTest {
         Converter SUT = new Converter();
 
         // ACT
-        String result = SUT.convert(mockedMedia);
+        String result = SUT.convertMedia(mockedMedia);
 
         // ASSERT
         assertEquals("Should return empty JSON string when Media object is null", "", result);
@@ -33,10 +33,29 @@ public class ConverterTest {
         Converter SUT = new Converter();
 
         // ACT
-        String result = SUT.convert(m);
+        String result = SUT.convertMedia(m);
 
         // ASSERT
         assertEquals("Should return correct JSON string", expected, result);
     }
 
+    @Test
+    public void shouldReturnJSONStringOfMetadataParameters() throws WrongFormatException, NoDataItemsException {
+        // ARRANGE
+        String _strategy = "DFS";
+        int _numberOfPage = 5;
+        int _timeElapsed = 600;
+        int _searchDepth = 3;
+
+        String expected = "{\"numberOfPage\":\"5\",\"searchDepth\":\"3\",\"strategy\":\"DFS\",\"timeElapsed\":\"600\"}";
+
+        // Create SUT
+        Converter SUT = new Converter();
+
+        // ACT
+        String result = SUT.convertMetadata(_strategy,_numberOfPage,_timeElapsed,_searchDepth);
+
+        // ASSERT
+        assertEquals("Should return correct JSON string", expected, result);
+    }
 }
