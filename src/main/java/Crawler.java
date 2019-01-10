d1d051021c09c25bc61a5c76fed133bf298df438
Amelia Kezia Gunawan,Amel A <a.kezia@student.fontys.nl>
2019-01-10 11:47:57 +0100

    Crawler : baseAddressSyntaxIsCorrect

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -1,11 +1,16 @@
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class Crawler {
     private String base_address, output;
 
-    public Crawler(String base_address) throws NoDataItemsException {
-        Boolean checkBaseAddress;
-        checkBaseAddress = base_address.trim().isEmpty();
-
-        if(!checkBaseAddress){
+    public Crawler(String base_address) throws NoDataItemsException, MalformedURLException, URISyntaxException {
+        Boolean checkEmptyBaseAddress;
+        checkEmptyBaseAddress = base_address.trim().isEmpty();
+        URL url = new URL(base_address);
+        url.toURI();
+        if(!checkEmptyBaseAddress){
             this.base_address = base_address;
         } else{
             throw new NoDataItemsException("Base Address shoud not be Empty!");
--- src/test/java/CrawlerTest.java
+++ src/test/java/CrawlerTest.java
@@ -2,6 +2,13 @@ import org.jsoup.Jsoup;
 import org.junit.Assert;
 import org.junit.Test;
 
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class CrawlerTest {
     @Test
     public void connectionShouldEstablished(){
@@ -10,16 +17,24 @@ public class CrawlerTest {
     }
 
     @Test
-    public void shouldHasBaseAddress() throws NoDataItemsException {
-        String baseAddress = "http://www.google.com";
-        Crawler crawler = new Crawler(baseAddress);
+    public void shouldHasBaseAddress() throws NoDataItemsException, MalformedURLException, URISyntaxException {
+        String base_address = "http://www.google.com";
+        Crawler crawler = new Crawler(base_address);
         Assert.assertNotNull(crawler.getBaseAddress());
     }
 
     @Test(expected = NoDataItemsException.class)
-    public void baseAddressIsNotEmpty() throws NoDataItemsException {
-        String baseAddress = "";
-        Crawler crawler = new Crawler(baseAddress);
+    public void baseAddressIsNotEmpty() throws NoDataItemsException, MalformedURLException, URISyntaxException {
+        String base_address = "";
+        Crawler crawler = new Crawler(base_address);
+    }
+
+
+    @Test
+    public void baseAddressSyntaxIsCorrect() throws MalformedURLException, URISyntaxException, NoDataItemsException {
+        String base_address = "http://www.google.com";
+        Crawler crawler = new Crawler(base_address);
+
     }
 
 
