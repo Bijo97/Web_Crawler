2f1e103b2ed9836526f32038c51d7dff19065687
Amelia Kezia Gunawan,Amel A <a.kezia@student.fontys.nl>
2019-01-10 11:28:44 +0100

    Crawler : shouldHasBaseAddress + baseAdressIsNotEmpty

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -1,7 +1,23 @@
 public class Crawler {
-    private String output;
+    private String base_address, output;
+
+    public Crawler(String base_address) throws NoDataItemsException {
+        Boolean checkBaseAddress;
+        checkBaseAddress = base_address.trim().isEmpty();
+
+        if(!checkBaseAddress){
+            this.base_address = base_address;
+        } else{
+            throw new NoDataItemsException("Base Address shoud not be Empty!");
+        }
+
+    }
 
     public String getOutput() {
         return output;
+    }
+
+    public String getBaseAddress() {
+        return base_address;
     }
 }
--- src/test/java/CrawlerTest.java
+++ src/test/java/CrawlerTest.java
@@ -1,4 +1,5 @@
 import org.jsoup.Jsoup;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CrawlerTest {
@@ -7,5 +8,19 @@ public class CrawlerTest {
         String url = "http://www.google.com";
         Jsoup.connect(url);
     }
+
+    @Test
+    public void shouldHasBaseAddress() throws NoDataItemsException {
+        String baseAddress = "http://www.google.com";
+        Crawler crawler = new Crawler(baseAddress);
+        Assert.assertNotNull(crawler.getBaseAddress());
+    }
+
+    @Test(expected = NoDataItemsException.class)
+    public void baseAddressIsNotEmpty() throws NoDataItemsException {
+        String baseAddress = "";
+        Crawler crawler = new Crawler(baseAddress);
+    }
+
 
 }
