38a6b34e31deda72552b503fd2738ec65b7303d3
Albert Pratomo <i406448@fhict.nl>
2019-01-13 22:20:06 +0100

    MusicTest: Reformat Act-Arrange-Asset

--- src/test/java/MusicTest.java
+++ src/test/java/MusicTest.java
@@ -10,21 +10,27 @@ public class MusicTest {
     Media media;
     @Test
     public void shouldHaveArtist() throws WrongFormatException, NoDataItemsException {
+        //ARRANGE
         media = new Media(genre,format,year,name);
-
         String artist = "Ludwig van Beethoven";
 
+        //ACT
         Music music = new Music(media, artist);
 
+        //ASSERT
         Assert.assertEquals("Ludwig van Beethoven", music.getArtist());
     }
 
     @Test (expected = NoDataItemsException.class)
     public void artistIsNotEmpty() throws WrongFormatException, NoDataItemsException {
+        //ARRANGE
         media = new Media(genre,format,year,name);
         String artist = "";
 
+        //ACT
         Music music = new Music(media, artist);
+
+        //ASSERT
     }
 
 
