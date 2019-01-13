28497627563072b64843818b7b8db2e77d5378a9
Albert Pratomo <i406448@fhict.nl>
2019-01-13 22:16:17 +0100

    MediaTest: Reformat Act-Arrange-Asset

--- src/test/java/MediaTest.java
+++ src/test/java/MediaTest.java
@@ -5,13 +5,16 @@ import org.junit.Test;
 public class MediaTest {
     @Test
     public void shouldHaveGenreFormatYearAndName() throws NoDataItemsException, WrongFormatException {
+        //ARRANGE
         String genre = "comedy";
         String format = "Blu-ray";
         String year = "1999";
         String name = "Office Space";
 
+        //ACT
         Media media = new Media(genre,format,year,name);
 
+        //ASSERT
         Assert.assertNotNull(media.getGenre());
         Assert.assertNotNull(media.getFormat());
         Assert.assertNotNull(media.getYear());
@@ -20,21 +23,29 @@ public class MediaTest {
 
     @Test (expected = NoDataItemsException.class)
     public void genreFormatYearAndNameAreNotEmpty() throws NoDataItemsException, WrongFormatException {
+        //ARRANGE
         String genre = "comedy";
         String format = "Blu-ray";
         String year = "  ";
         String name = "Office Space";
 
+        //ACT
         Media media = new Media(genre,format,year,name);
+
+        //ASSERT
     }
 
     @Test (expected = WrongFormatException.class)
     public void yearShouldBeStringOf4Number() throws NoDataItemsException, WrongFormatException {
+        //ARRANGE
         String genre = "comedy";
         String format = "Blu-ray";
         String year = "1997a";
         String name = "Office Space";
 
+        //ACT
         Media media = new Media(genre,format,year,name);
+
+        //ASSERT
     }
 }
