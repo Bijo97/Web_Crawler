127a9dc45d62f4b9f021bccc6d49d1e3b59fe0a5
Albert Pratomo <i406448@fhict.nl>
2019-01-13 18:39:07 +0100

    Crawler: couldExtractPageLinksFromBaseAddress

--- src/test/java/CrawlerTest.java
+++ src/test/java/CrawlerTest.java
@@ -2,7 +2,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
 import org.junit.Assert;
 import org.junit.Test;
-
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
@@ -13,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -22,7 +22,7 @@ public class CrawlerTest{
     @Test
     public void shouldHaveBaseAddress() throws NoDataItemsException, MalformedURLException, URISyntaxException {
         // ARRANGE
-        String base_address = "http://www.google.com";
+        String base_address = "files/Catalog.html";
 
         // Create SUT
         Crawler SUT = new Crawler(base_address);
@@ -34,9 +34,29 @@ public class CrawlerTest{
     }
 
     @Test
+    public void couldExtractPageLinksFromBaseAddress() throws URISyntaxException, NoDataItemsException, IOException {
+        // ARRANGE
+        String base_address = "files/Catalog.html";
+        // Create mockScrapper
+        Scrapper mockScrapper = mock(Scrapper.class);
+        //Specify behaviours
+
+        // Create SUT
+        Crawler SUT = new Crawler(base_address);
+
+        // ACT
+        SUT.setScrapper(mockScrapper);
+        SUT.run();
+        int pagesFound = SUT.getPagesFound();
+
+        // ASSERT
+        assertTrue("Page links found should be at least 1",pagesFound >= 1);
+    }
+
+    @Test
     public void scrapperScrapMethodIsCalledAsManyAsPagesFound() throws NoDataItemsException, MalformedURLException, URISyntaxException {
         // ARRANGE
-        String base_address = "http://www.google.com";
+        String base_address = "files/Catalog.html";
         // Create mockScrapper
         Scrapper mockScrapper = mock(Scrapper.class);
         //Specify behaviours
@@ -53,7 +73,7 @@ public class CrawlerTest{
         verify(mockScrapper, atLeast(pagesFound)).scrapping();
     }
 
-    @Test(expected = MalformedURLException.class)
+    @Test(expected = NoDataItemsException.class)
     public void ifBaseAddressIsEmptyThrowException() throws NoDataItemsException, MalformedURLException, URISyntaxException {
         //ARRANGE
         String base_address = "";
@@ -66,11 +86,5 @@ public class CrawlerTest{
         //ASSERT
     }
 
-    @Test
-    public void couldExtractURLsUsingBaseAddress() throws URISyntaxException, NoDataItemsException, IOException {
-        String base_address = "http://localhost/tci/";
-        Crawler crawler = new Crawler(base_address);
-        List<String> urls = new ArrayList<>();
-        Assert.assertTrue(crawler.visitPage(base_address));
-    }
+
 }
